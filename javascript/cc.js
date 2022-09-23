a0c6f84558171ae55aa1c454e7913955ee933b4d
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2022-09-23 21:40:40 +0200

    code formatted

--- javascript/cc.js
+++ javascript/cc.js
@@ -1,23 +1,19 @@
 {
-    const formElement = document.querySelector(".js-form");
+  const formElement = document.querySelector(".js-form");
 
-    const currencyElement = document.querySelector(".js-currencyIn");
-  
-    const resultElement = document.querySelector(".js-currencyResult");
-  
-    const selectedElement = document.querySelector(".js-currencySelected");
+  const currencyElement = document.querySelector(".js-currencyIn");
+
+  const resultElement = document.querySelector(".js-currencyResult");
+
+  const selectedElement = document.querySelector(".js-currencySelected");
 
   const calculate = (currency, currencyRate) => {
-    
-
-    return currencyElement.value/ selectedElement.value;
+    return currencyElement.value / selectedElement.value;
   };
 
   formElement.addEventListener("input", () => {
     const result = calculate(currencyElement.value, selectedElement.value);
 
-    resultElement.innerText = result.toFixed(1)
+    resultElement.innerText = result.toFixed(1);
   });
 }
-
-
